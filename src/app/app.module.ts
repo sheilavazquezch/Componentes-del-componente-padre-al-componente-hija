ff4315aeff0f2331b798e217845e0743abbd2dcc
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-06-25 21:10:16 -0500

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { DadoComponent } from './dado/dado.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadoComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
